18d8ea03933f05512f930a3118a9976c9acebd07
jwxhhxx <774578034@qq.com>
2019-06-02 17:25:49 +0800

    jstest 数据类型

---
+++ jstest/js/main.js
@@ -0,0 +1,20 @@
+//Number 数字类型
+var a = 123; //整型
+var b = 1.1; //浮点型
+var d = NaN; //Not a Number
+var e = Infinity; //无穷大
+//String 字符串类型
+var aa = 'string';
+var bb = "string2";
+var cc = `string3`;
+//Boolean 布尔值类型
+var t = true;
+var f = false;
+//Array 数组类型
+var arr = ['a', 'b', 'c', 123, 321, true];
+//Object 对象类型
+var obj = {
+    name: 'name',
+    value: 10,
+    haa: 'haa'
+};
