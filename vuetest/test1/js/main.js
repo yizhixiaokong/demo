ecdcb4a5b1e7d0eaefcb353e9ee87d95cedc5eec
yizhixiaokong <wdhxx20121@126.com>
2020-02-20 15:44:55 +0800

    vuetest 基本语法，v-model，v-if，v-show

--- vuetest/test1/js/main.js
+++ vuetest/test1/js/main.js
@@ -3,6 +3,10 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         hi: '妈妈咪呀！',
-        name: 'hahaha'
+        name: 'hahaha',
+        age: '18',
+        sex: 'male',
+        money: '100',
+        like: 'cake'
     }
 })
