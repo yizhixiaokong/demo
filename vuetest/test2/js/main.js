1bbb117f06ac3bf872c13f2b03ecebd22c27351f
yizhixiaokong <wdhxx20121@126.com>
2020-02-20 16:14:01 +0800

    vuetest v-for

---
+++ vuetest/test2/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        foodList: [{
+                name: '苹果',
+                price: 100,
+            },
+            {
+                name: '梨子',
+                price: 120,
+            },
+            {
+                name: '香蕉',
+                price: 80,
+            },
+        ]
+    }
+});
