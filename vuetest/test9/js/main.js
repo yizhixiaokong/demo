68ff84ae2714cc05dcbd12e5e991a4b226d17442
yizhixiaokong <wdhxx20121@126.com>
2020-02-21 18:12:11 +0800

    vuetest 组件实例

---
+++ vuetest/test9/js/main.js
@@ -0,0 +1,31 @@
+Vue.component('like', {
+    template: `
+        <button :class="{liked: liked}" @click="toggle_like()">
+            👍 {{like_count}}
+        </button>
+    `,
+    data: function() {
+        return {
+            like_count: 10,
+            liked: false,
+        }
+    },
+    methods: {
+        toggle_like: function() {
+            if (!this.liked) {
+                this.like_count++;
+            } else {
+                this.like_count--;
+            }
+            this.liked = !this.liked;
+        }
+    },
+})
+
+
+
+
+
+var app = new Vue({
+    el: '#app',
+});
