065c66c21ae0b2a707b2131a25f54a18fb0b1b51
yizhixiaokong <wdhxx20121@126.com>
2020-02-21 15:24:45 +0800

    vuetest v-model多种元素下的用法

---
+++ vuetest/test6/js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        sex: 'female',
+        fruit: ['orange'],
+        article: 'somesomesome',
+        from: null,
+        destination: [],
+    },
+    methods: {
+
+    },
+});
