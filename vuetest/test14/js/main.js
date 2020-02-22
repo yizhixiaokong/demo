239d44682e35b1f33613a753be0c4c6061c10925
yizhixiaokong <wdhxx20121@126.com>
2020-02-22 15:57:33 +0800

    vuetest 自定义指令传参

--- vuetest/test14/js/main.js
+++ vuetest/test14/js/main.js
@@ -1,11 +1,20 @@
 Vue.directive('pin', function(el, binding) {
     var pinned = binding.value;
+    var position = binding.modifiers;
+    var warning = binding.arg;
     if (pinned) {
         el.style.position = 'fixed';
-        el.style.top = '10px';
-        el.style.left = '10px';
+        for (var key in position) {
+            if (position[key]) {
+                el.style[key] = '10px';
+            }
+        }
+        if (warning === 'true') {
+            el.style.background = 'yellow';
+        }
     } else {
         el.style.position = 'static';
+        el.style.background = 'azure';
     }
 })
 var app = new Vue({
