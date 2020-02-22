a9671e76d39eeb4b09e07e65fe4deec0a62f344b
yizhixiaokong <wdhxx20121@126.com>
2020-02-22 19:19:17 +0800

    vue-routertest 子路由

--- vuetest/vue-routertest1/js/app.js
+++ vuetest/vue-routertest1/js/app.js
@@ -22,6 +22,30 @@ var routes = [{
                 `
         }
     },
+    {
+        path: '/user/:name',
+        component: {
+            template: `
+                    <div>
+                        <div>我叫：{{$route.params.name}}</div>
+                        <div>我今年：{{$route.query.age}}岁</div>
+                        <router-link :to="'/user/' + $route.params.name + '/more'">更多1</router-link>
+                        <router-link to="more" append>更多信息2</router-link>
+                        <router-view></router-view>
+                    </div>
+                `
+        },
+        children: [{
+            path: 'more',
+            component: {
+                template: `
+                        <div>
+                            用户：{{$route.params.name}}的详细信息
+                        </div>
+                    `
+            }
+        }]
+    },
 ];
 
 
