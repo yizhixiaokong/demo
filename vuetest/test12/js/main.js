d5931b4896a20dcb1c9a0d32c19290a88036110c
yizhixiaokong <wdhxx20121@126.com>
2020-02-22 14:28:18 +0800

    vuetest 平行组件通信

---
+++ vuetest/test12/js/main.js
@@ -0,0 +1,40 @@
+var Event = new Vue();
+
+Vue.component('hxx', {
+    template: `
+        <div>
+            hxx：<input @keyup="on_change" v-model="i_said"/>
+        </div>
+    `,
+    methods: {
+        on_change: function() {
+            Event.$emit('hxx-said-something', this.i_said);
+        }
+    },
+    data: function() {
+        return {
+            i_said: '',
+        }
+    },
+})
+Vue.component('jwx', {
+    template: `
+        <div>
+            jwx:hxx说了{{hxx_said}}
+        </div>
+    `,
+    data: function() {
+        return {
+            hxx_said: '',
+        };
+    },
+    mounted: function() { //生命周期钩子之一
+        var me = this;
+        Event.$on('hxx-said-something', function(data) {
+            me.hxx_said = data;
+        });
+    }
+})
+var app = new Vue({
+    el: '#app',
+});
