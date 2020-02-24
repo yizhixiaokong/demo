43b82084426bd346652c0b07657b58505e3be41c
yizhixiaokong <wdhxx20121@126.com>
2020-02-24 15:47:18 +0800

    vuetodolist 封装localStorage

---
+++ vuetodolist/js/myStorage.js
@@ -0,0 +1,23 @@
+;
+(function() {
+    window.ms = {
+        set: set,
+        get: get,
+    };
+
+    function set(key, val) {
+        localStorage.setItem(key, JSON.stringify(val));
+    }
+
+    function get(key) {
+        var json = localStorage.getItem(key);
+        if (json) {
+            return JSON.parse(json);
+        }
+
+    }
+})();
+
+ms.set('name', 'hxx');
+var name = ms.get('name');
+console.log('name', name);
