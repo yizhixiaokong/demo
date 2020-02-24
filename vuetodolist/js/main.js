b2c01c31456d5ce40b030aaad7927f6cf505d9c8
yizhixiaokong <wdhxx20121@126.com>
2020-02-24 18:48:41 +0800

    vuetodolist 修复了小bug，添加详情的显示

--- vuetodolist/js/main.js
+++ vuetodolist/js/main.js
@@ -2,19 +2,74 @@
 (function() {
     'use strict';
 
+    var Event = new Vue();
+
     function copy(obj) {
         return Object.assign({}, obj);
     }
+    Vue.component('task', {
+        template: '#task-tpl',
+        props: ['todo'],
+        methods: {
+            action: function(name, params) {
+                Event.$emit(name, params);
+            }
+        }
+    });
+
     new Vue({
         el: '#main',
         data: {
             list: [],
+            last_id: 0,
             current: {},
         },
         mounted: function() {
+            var me = this;
             this.list = ms.get('list') || this.list;
+            this.last_id = ms.get('last_id') || this.last_id;
+
+            setInterval(function() {
+                me.check_alert();
+            }, 1000);
+
+            Event.$on('remove', function(id) {
+                if (id) {
+                    me.remove(id);
+                }
+            });
+            Event.$on('toggle_complete', function(id) {
+                if (id) {
+                    me.toggle_complete(id);
+                }
+            });
+
+            Event.$on('set_current', function(id) {
+                if (id) {
+                    me.set_current(id);
+                }
+            });
+            Event.$on('toggle_detail', function(id) {
+                if (id) {
+                    me.toggle_detail(id);
+                }
+            });
         },
         methods: {
+            check_alert: function() {
+                var me = this;
+                this.list.forEach(function(row, index) {
+                    var alert_at = row.alert_at;
+                    if (!alert_at || row.alert_confirmed) return;
+                    var alert_at = (new Date(alert_at)).getTime();
+                    var now = (new Date()).getTime();
+
+                    if (now >= alert_at) {
+                        var confirmed = confirm(row.title);
+                        Vue.set(me.list[index], 'alert_confirmed', confirmed);
+                    }
+                });
+            },
             marge: function() {
                 var is_update, id;
                 is_update = id = this.current.id;
@@ -25,7 +80,9 @@
                     var title = this.current.title;
                     if (!title && title !== 0) return;
                     var todo = copy(this.current);
-                    todo.id = this.next_id();
+                    this.last_id++;
+                    ms.set('last_id', this.last_id);
+                    todo.id = this.last_id;
                     this.list.push(todo);
                 }
                 this.reset_current();
@@ -51,6 +108,10 @@
             toggle_complete: function(id) {
                 var index = this.find_index(id);
                 Vue.set(this.list[index], 'completed', !this.list[index].completed);
+            },
+            toggle_detail: function(id) {
+                var index = this.find_index(id);
+                Vue.set(this.list[index], 'show_detail', !this.list[index].show_detail);
             },
         },
         watch: {
